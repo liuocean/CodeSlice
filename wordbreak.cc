0ced1785a8f9a07e89b1207a88192f91199565b2
liuocean <1059532226@qq.com>
2014-04-21 10:59:20 +0800

    wordbreak.cc

---
+++ wordbreak.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution{
+public:
+	bool wordBreak(string s, unordered_set<string> &dict){
+		if(s.length() == 0)
+			return true;
+		vector<bool>res(s.length()+1, false);
+		res[0]=true;
+		for(int i=1; i<=s.length(); i++){
+			for(int j=0; j<i; j++){
+				if(res[j] && dict.find(s.substr(j, i-j)) != dict.end() ){
+						res[i]=true; 
+						continue;
+				}
+			}
+		}
+		return res[s.length()];
+	}
+};
+
+int main(){
+	string str="leetcode";
+
+	unordered_set<string>dict;
+	dict.insert("lee");
+	dict.insert("leet");
+	dict.insert("cdo");
+	dict.insert("code");
+
+	Solution solution;
+	
+	bool flag=solution.wordBreak(str, dict);
+	if(flag)
+		cout<<"true"<<endl;
+	else cout<<"false"<<endl;
+	return 0;
+
+}
