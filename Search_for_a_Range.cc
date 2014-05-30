24401b37a0c8ac9264301fcfb4b19696a0f57c1c
liuocean <1059532226@qq.com>
2014-05-30 16:41:05 +0800

    add

--- Search_for_a_Range.cc
+++ Search_for_a_Range.cc
@@ -1,3 +1,16 @@
+/*
+Search for a Range 
+Given a sorted array of integers, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -15,7 +28,7 @@ int Solution::leftBorder(int A[], int n, int target){
         return -1;
     int low=0, high=n-1;
     while(low < high){
-       int mid=low + ((high -low) >> 1); 
+       int mid=low + ((high -low) >> 1); ////一定注意移位运算符的优先级低于算术运算符
        if(A[mid]==target) 
             if(mid==0 || A[mid-1]!=target)
               return mid;
@@ -33,7 +46,7 @@ int Solution::rightBorder(int A[], int n, int target){
         return -1;
     int low=0, high=n-1;
     while(low < high){
-       int mid=low + ((high -low) >> 1); 
+       int mid=low + ((high -low) >> 1);  //一定注意移位运算符的优先级低于算术运算符
        if(A[mid]==target) 
             if(mid==high || A[mid+1]!=target)
               return mid;
