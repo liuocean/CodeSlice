a07baf793ecaa152a5df649ab91cac88001aea8a
oceaninit <oceaninit@gmail.com>
2017-07-23 11:03:48 +0800

    add metaclass

---
+++ python/metaclass.py
@@ -0,0 +1,32 @@
+Person = type('Person', (object,), {'name':'baby', 'age':20})
+print type(Person())
+
+class UpperMetaClass(type):
+
+	def __new__(cls, name, bases, attrs):
+		print '__new__', cls, name, bases, attrs
+		# attrs['**'] = 'OO' # new attr
+		attrs_tuple = ((attr, val) for attr, val in attrs.iteritems() if not attr.startswith('__'))
+		upper_attrs = dict((attr.upper(), val) for attr, val in attrs_tuple) 
+		# return type.__new__(cls, name, bases, attrs)
+		return super(UpperMetaClass, cls).__new__(cls, name, bases, attrs)
+
+class UpperClass(object):
+	__metaclass__ = UpperMetaClass
+
+upper = UpperClass()
+
+
+def upper_func(name, bases, attrs):
+	attrs_tuple = ((attr, val) for attr, val in attrs.iteritems() if not attr.startswith('__'))
+	upper_attrs = dict((attr.upper(), val) for attr, val in attrs_tuple) 
+	return type(name, bases, attrs)
+
+class UpperClass1(object):
+	__metaclass__ = upper_func
+
+
+__metaclass__ = upper_func  #作用到模块中的所有类
+
+class classA(list): 
+	pass
